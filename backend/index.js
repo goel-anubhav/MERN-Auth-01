f588d310c78ff19a658fc38ee5e8d085e00d549c
goel-anubhav <anubhavgoyal12062001@gmail.com>
2024-09-28 13:09:46 +0530

    Added Routes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectdb } from "./db/connectdb.js";
+import authRoutes from "./routes/auth.route.js";
 //CwfajWTkMwOTffKG
 
 dotenv.config();
@@ -9,6 +10,11 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello World 1223@");
 });
+
+app.use("/api/auth", authRoutes);
+//  => {
+//   res.send("Hello World 1223@");
+// });
 
 app.listen(3000, () => {
   connectdb();
