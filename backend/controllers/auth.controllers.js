f5e8b24a635a7d4ce214f24aa9f08203dc095d01
goel-anubhav <anubhavgoyal12062001@gmail.com>
2024-09-30 15:17:30 +0530

    Logout Functionality Achieved

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -87,5 +87,6 @@ export const login = async (req, res) => {
 };
 
 export const logout = async (req, res) => {
-  res.send("logout page");
+  res.clearCookie("token");
+  res.status(200).json({success:true, message:"Logout Successfully"})
 };
