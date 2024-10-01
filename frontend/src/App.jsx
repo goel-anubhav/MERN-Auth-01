168a8044547965d5d92994ec401d6addb7e82147
goel-anubhav <anubhavgoyal12062001@gmail.com>
2024-10-01 14:50:31 +0530

    Routes Added In the Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import FloatingCircle from "./components/FloatingShape/FloatingCircle";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoginPage from "./components/Pages/LoginPage";
+import SignUpPage from "./components/Pages/SignUpPage";
 
 function App() {
   return (
-    <div
-      className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 
-    to-emerald-900 flex items-center justify-center relative overflow-hidden"
-    >
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
       <FloatingCircle />
+      <Router>
+        <Routes>
+          <Route path="/" element={<div> Home</div>} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
-
 export default App;
