b81b11a61dcc41b4797626166700161807f602be
goel-anubhav <anubhavgoyal12062001@gmail.com>
2024-10-15 14:13:34 +0530

    Verify Email Layout Done

--- frontend/src/Pages/EmailVerificationPage.jsx
+++ frontend/src/Pages/EmailVerificationPage.jsx
@@ -1,7 +1,60 @@
-import React from "react";
-
+import { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
 const EmailVerificationPage = () => {
-  return <div>Email</div>;
+  const [code, setcode] = useState(["", "", "", "", "", ""]);
+  const inputRefs = useRef([]);
+  const navigate = useNavigate();
+  const handleChange = (index, value) => {};
+  const handleKeyDown = (index, e) => {};
+  const isLoading = false;
+  return (
+    <div
+      className="max-w-md w-full  bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl 
+    rounded-2xl shadow-xl overflow-hidden"
+    >
+      <motion.div
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        className="max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl
+        overflow-hidden p-8 w-full"
+      >
+        <h2
+          className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500
+        text-transparent bg-clip-text"
+        >
+          Verify Your Email
+        </h2>
+        <form className="space-y-6">
+          <div className="flex justify-between">
+            {code.map((digit, index) => (
+              <input
+                key={index}
+                ref={(el) => (inputRefs.current[index] = el)}
+                type="text"
+                maxLength="6"
+                value={digit}
+                onChange={(e) => handleChange(index, e.target.value)}
+                onKeyDown={(e) => handleKeyDown(index, e)}
+                className="w-12 h-12 text-center text-2xl font-bold bg-gray-700 text-white-border-2 border-gray-600
+                rounded-lg focus:border-green-500 focus:outline-none"
+              />
+            ))}
+          </div>
+          <motion.button
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            type="submit"
+            disabled={isLoading || code.some((digit) => !digit)}
+            className="w-full bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold py-3 px-4 rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 disabled:opacity-50"
+          >
+            {isLoading ? "Verifying..." : "Verify Email"}
+          </motion.button>
+        </form>
+      </motion.div>
+    </div>
+  );
 };
 
 export default EmailVerificationPage;
